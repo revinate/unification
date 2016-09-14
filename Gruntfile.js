b759c7bab1cd47490c26b40caaaaeb20d44f7f3a
jeffmcfarland <jeff@revinate.com>
2016-09-14 13:49:20 -0700

    no jira

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,5 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
-		styledown: {
-			build: {
-				files: {
-					'index.html': ['styleguide-markdown.md'],
-					'principles.html': ['principles-markdown.md'],
-					'resources.html': ['resources-markdown.md']
-				},
-				options: {
-					config: 'styleguide-config.md',
-					title: 'Revinate Styleguide'
-				}
-			}
-		},
-		serve: {
-			options: {
-				port: 9000
-			}
-		}
-	});
-
 	grunt.loadNpmTasks('grunt-styledown');
 	grunt.loadNpmTasks('grunt-serve');
 
